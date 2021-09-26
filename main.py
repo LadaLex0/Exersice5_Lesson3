8ba60774a4d05292080e029df7c37d69a618ad01
LadaLex0 <91010364+LadaLex0@users.noreply.github.com>
2021-09-26 18:29:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+a = int(input('Print 1st number: '))
+b = int(input('Print 2nd number: '))
+c = int(input('Print 3rd number: '))
+if a>b>c:
+    print('The largest number is', a)
+elif b>c:
+    print('The largest number is', b)
+else: print('The largest number is', c)
